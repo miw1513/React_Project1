f53f2a8937d7a685a1e49471d593f3ab305800d0
miw1513 <miw1513@hotmail.com>
2017-06-14 13:51:40 +0700

    ง
    ง

--- src/Footer.js
+++ src/Footer.js
@@ -1,10 +1,23 @@
 import React, {Component} from "react"
 
 class Footer extends Component{
-    
+
+    constructor(props){
+        super(props)
+        this.state = {
+            timing: 0
+        }
+        setInterval(()=>{
+            this.setState({
+                timing: this.state.timing+1
+            })
+        },1000)
+    }
+
     render(){
+        let {timing} = this.state
         return (
-            <div> Footer </div>
+            <div> Footer Second : {timing}</div>
         )
     }
 }
