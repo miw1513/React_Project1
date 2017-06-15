3a59caedf86a225e5584f3c535629098eb556f32
miw1513 <miw1513@hotmail.com>
2017-06-16 01:27:26 +0700

    updated
    updated

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import Header from "./header"
-import Footer from "./Footer"
+import TodoInput from './TodoInput';
+import Todolist from './Todolist';
 
 class App extends Component {
   constructor(props){
--- src/TodoInput.js
+++ src/TodoInput.js
@@ -9,7 +9,7 @@ class TodoInput extends Component {
 
 
 
-        )
+        );
     }
 }
 
