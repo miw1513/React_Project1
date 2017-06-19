7b6f5c2765e6e7122c092e24200f6de0d8add221
miw1513 <miw1513@hotmail.com>
2017-06-19 21:43:34 +0700

    updated import route
    updated import route

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
+import { BrowserRouter as Router , Link , Route} from 'react-router-dom'
+import Home from './Component/Home'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+   <App/>, document.getElementById('root'));
 registerServiceWorker();
